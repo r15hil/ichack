923bf5a31c9d96f2fb6fb4defe5b86c738d54cfa
r15hil <rishilapple@gmail.com>
2022-02-05 16:51:52 +0000

    append

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,35 @@
 import logo from './logo.svg';
-import React, { useCallback, useEffect } from "react";
+import React, { useCallback, useEffect, useState, setState } from "react";
 import './App.css';
 
 // document.addEventListener('keydown', function(event){
 //   console.log(`Key: ${event.key} with keycode ${event.keyCode} has been pressed`);)
 
+
+
 function App() {
+
+  // const [toggleMobileNavbar, setToggleMobileNavbar] = useState(false)
+
+  // const handleToggleNavBar = () => {
+  //     setToggleMobileNavbar(!toggleMobileNavbar)
+  // }
+
+  // const handleCloseNavBar = () => {
+  //     setToggleMobileNavbar(false)
+  // }
+
+let [value, setValue] = useState('hello')
+// this.state = ''
+
   
   const handleKeyPress = (event) => {
-    if(event.key === "y"){
-      console.log('enter press here! ')
+    if(event.key === 'y'){ //backspace //13 enter 32 space
+      console.log(event.key)
     }
+
+    setValue(value + event.key)
+    console.log(event.key)
   }
 
   useEffect(() => {
@@ -20,13 +39,29 @@ function App() {
       document.removeEventListener("keydown", handleKeyPress);
     };
   }, [handleKeyPress]);
+  
+  
+
+  const textToType = ["Beef with Jeef", "Poo", "Pee", "PooPoo", "PeePee"];
+  
+  // function TextToTypeList (props) {
+  //   const textList = props.textList;
+  //   const listItems = textList.map((text) =>
+  //     <li>{text}</li>
+  //   );
+  //   return (
+  //     <ul>{listItems}</ul>
+  //   );
+  // }
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        
+        {/* <TextToTypeList numbers={textToType} /> */}
         <p>
-          Beef with Jeef
+        {value}
         </p>
         <form>
   
