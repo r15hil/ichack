a5430df1b8529ed26f2b418e6da3b81e697466d6
r15hil <rishilapple@gmail.com>
2022-02-05 16:29:09 +0000

    Added event

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,45 @@
 import logo from './logo.svg';
+import React, { useCallback, useEffect } from "react";
 import './App.css';
 
+// document.addEventListener('keydown', function(event){
+//   console.log(`Key: ${event.key} with keycode ${event.keyCode} has been pressed`);)
+
 function App() {
+  
+  const handleKeyPress = (event) => {
+    if(event.key === "y"){
+      console.log('enter press here! ')
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener("keydown", handleKeyPress);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyPress);
+    };
+  }, [handleKeyPress]);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Beef with Jeef
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <form>
+  
+        </form>
+              <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Learn React
+              </a>
+            </header>
     </div>
   );
 }
